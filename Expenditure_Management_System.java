939e19219180a6078091d1a3300c5a3347047ae6
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2025-07-07 14:23:03 +0000

    Developed Menu for the expenditure app

---
+++ Expenditure_Management_System.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Expenditure_Management_System {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("------Welcome To Nkwa Tracking App------\n");
+        System.out.print("Select an option:\n1. Add Expenditure\n2. View Expenditure\n3. Manage Categories" +
+                "\n4. Manage Categories\n5. Bank Overview\n6. Search and Sort Expenditures\n" +
+                "7.Generate report\n8. Exit\n:  ");
+
+        int choice = input.nextInt();
+
+
+
+    }
+
+
+}
