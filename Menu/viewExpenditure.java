21cb01fad72dbff07a908af51972445a6d64f289
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2025-07-12 22:50:33 +0000

    find an expenditure item based on user specification

---
+++ Menu/viewExpenditure.java
@@ -0,0 +1,12 @@
+package Menu;
+
+public class viewExpenditure {
+    public static void searchExpenditure(){
+        System.out.println("1. List all (chronological)\n" +
+                "2. Sort by category (A‑Z)\n" +
+                "3. Search by date range\n" +
+                "4. Search by category\n" +
+                "5. Search cost range\n" +
+                "6. Search by bank account");
+    }
+}
