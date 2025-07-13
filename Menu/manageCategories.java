22e32e7cbf6c8644307ac33418021ba3a30325f4
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2025-07-13 23:18:18 +0000

    Options to perfrom in manage categories

--- Menu/manageCategories.java
+++ Menu/manageCategories.java
@@ -1,4 +1,39 @@
 package Menu;
+import java.util.Scanner
 
 public class manageCategories {
+    public static void categories(){
+        Scanner s = new Scanner(System.in);
+        System.out.println("1. List Categories" +
+                "2. Add Category" +
+                "3. Delete Category");
+
+
+        String choice = s.nextLine();
+
+        switch (choice){
+            case "1" :
+                listcategories();
+            case "2":
+                addcategory();
+            case "3":
+                deletecategory();
+        }
+    }
+
+    public static void listcategories(){
+
+
+
+    }
+
+    public static void addcategory(){
+
+
+    }
+
+    public static void deletecategory(){
+
+    }
+
 }
